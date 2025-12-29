85020933450a8d5e7fc2d4ea024f7c9c80fcbe49
manas <manasvini569@revature.net>
2025-12-29 02:29:47 -0600

    Added SetController and made Set, SetRepository, and SetService work with Spring

--- BuilderService/src/main/java/org/example/Service/SetService.java
+++ BuilderService/src/main/java/org/example/Service/SetService.java
@@ -1,20 +1,36 @@
 package org.example.Service;
 
+import org.example.Controller.SetDTO;
 import org.example.Repository.ISetRepository;
-import org.example.Repository.JdbcSetRepository;
 import org.example.model.Set;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
+@Service
 public class SetService {
-    private final ISetRepository setRepo;
+    private final ISetRepository repository;
 
     public SetService(ISetRepository setRepo) {
-        this.setRepo = setRepo;
+        this.repository = setRepo;
     }
 
-    public List<Set> getAllSets() {
-        return setRepo.findAll();
+    public SetDTO getById(String id) {
+        Optional<Set> set = repository.findById(id);
+        return set.isEmpty() ? null : SetToDTO(set.get());
     }
 
+    public SetDTO getByName(String name) {
+        Optional<Set> set = repository.findByName(name);
+        return set.isEmpty() ? null : SetToDTO(set.get());
+    }
+
+    public List<SetDTO> getAllSets() {
+        return repository.findAll().stream().map(this::SetToDTO).toList();
+    }
+
+    private SetDTO SetToDTO(Set set) {
+        return new SetDTO(set.getId(), set.getName(), set.getReleaseYear());
+    }
 }
